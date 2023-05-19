2b6fbd96031aff8615b364b0b8c9ddaff266ec5f
Maciej <maciej.krupka@rad4m.com>
2023-05-19 14:09:05 +0200

    add Task2

--- tasks/task2.cpp
+++ tasks/task2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <timer.h>
 #include <thread>
 #include <future>
 #include <mutex>
@@ -30,42 +29,88 @@ std::vector<bool> pending_tasks(time_per_task.size(), false); //may be useful if
 
 
 void task(int time, int task) {
+    pending_tasks[task] = 1;
     std::this_thread::sleep_for (std::chrono::milliseconds(time));
-    completed_tasks[task] = true;
+    completed_tasks[task] = 1;
 }
 
-void run_single_threaded() {
+std::mutex task_mtx;
 
+int get_idx_task()
+{
+    std::lock_guard<std::mutex> lk_guard(task_mtx);
+    for (std::vector<int>::size_type i = 0; i != pending_tasks.size(); i++){
+        if( pending_tasks[i] != true )
+        {
+            pending_tasks[i] = true;
+            return int(i);
+        }
+    }
+    return -1;
+}
+void get_task()
+{
+    while (!std::all_of(completed_tasks.begin(), completed_tasks.end(), [](int i){return i;})){
+        int task_idx = get_idx_task();
+        if (task_idx != -1){
+            std::cout << "Time per task: " << time_per_task[task_idx] << std::endl;
+            task(time_per_task[task_idx], task_idx);
+            completed_tasks[task_idx] = true;
+        }
+    }
+}
+
+
+void run_single_threaded() {
+    for (std::vector<int>::size_type i = 0; i != time_per_task.size(); i++)
+    {
+        pending_tasks[i] = true;
+        std::cout << "Time per task: " << time_per_task[i] << std::endl;
+        task(time_per_task[i], int(i));
+        completed_tasks[i] = true;
+    }
 }
 
 void run_multi_threaded(int threads){
-
+    std::vector<std::thread> thread_pool;
+    for(int i = 0; i < threads; i++){
+        thread_pool.push_back(std::thread(get_task));
+    }
+    for ( auto &thread : thread_pool){
+        thread.join();
+    }
 }
 
 void reset_tasks() {
-    for(auto&& task : completed_tasks) {
-        task = false;
+    for (std::vector<int>::size_type i = 0; i != time_per_task.size(); i++)
+    {
+        completed_tasks[i] = false;
+        pending_tasks[i] = false;
     }
 }
 
 int main() {
-    Timer timer;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
     int threads = 8;
-
-    timer.start();
+    
+    start = std::chrono::system_clock::now();
     run_single_threaded();
-    timer.stop();
-
-    std::cout<<"Task have been completed for one thread! Execution time: " <<timer.elapsed()<< " miliseconds.\n";
-
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> one_thread = end - start;
+    std::cout<<"Task have been completed for one thread! Execution time: " << one_thread.count() << " miliseconds.\n";
+    
     reset_tasks();
-
-    timer.start();
+    
+    start = std::chrono::system_clock::now();
     run_multi_threaded(threads);
-    std::cout<<"Task have been completed for " <<threads<<" threads! Execution time: " <<timer.elapsed()<< " miliseconds.\n";
-    timer.stop();
-
-
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> multi_thread = end - start;
+    std::cout<<"Task have been completed for " <<threads<<" threads! Execution time: " << multi_thread.count() << " miliseconds.\n";
+    
+    std::string complete = (multi_thread < one_thread ) ? "Multi thread is faster: " : "Single thread is faster: ";
+    double times = (multi_thread < one_thread) ? one_thread / multi_thread : multi_thread / one_thread;
+    std::cout << complete << times << " times." << std::endl;
+    
     std::cout<<"Complete the task\n";
     return 0;
 }
